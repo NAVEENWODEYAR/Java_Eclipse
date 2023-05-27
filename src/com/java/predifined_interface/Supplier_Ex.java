965d22fe6c741037ae45dbb571f2b8518bec5a3e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-05-27 19:55:49 +0530

    Functional Interface

---
+++ src/com/java/predifined_interface/Supplier_Ex.java
@@ -0,0 +1,26 @@
+package com.java.predifined_interface;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.function.Supplier;
+
+public class Supplier_Ex implements Supplier<LocalTime> 
+{
+    // providing the implementation	
+		public LocalTime get() 
+		{
+			return LocalTime.now();
+		}
+	public static void main(String[] args) 
+	{
+		// Calling the get() using the class object
+		Supplier s = new Supplier_Ex();
+		System.out.println("Time- " +s.get());
+
+		// Using the Supplier interface
+		Supplier<LocalDate> s1 = () -> LocalDate.now();
+		System.out.println("Today is "+s1.get());
+	}
+
+}

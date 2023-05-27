1745a320733cea1fa09a0305d4c8ad6ea630afe5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-05-27 18:07:31 +0530

    GE

---
+++ src/com/java/Lambda/Lamda_Ex2.java
@@ -0,0 +1,20 @@
+package com.java.Lambda;
+
+interface Lambda_Ex2
+{
+	public abstract int add_No(int a, int b);
+}
+public class Lamda_Ex2 {
+
+	public static void main(String[] args) {
+		// interface method implementation
+		Lambda_Ex2 l = (a,b) -> {
+				System.out.println("Lambda Expression");
+				System.out.println("Sum of the given numbers: "+ (a+b));
+				return a;
+		};
+		l.add_No(4, 4);
+
+	}
+
+}

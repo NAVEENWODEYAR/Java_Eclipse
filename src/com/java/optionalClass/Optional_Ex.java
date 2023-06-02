e4d0afdd1e1db5667f6af6472bda989aee13318d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-02 21:37:27 +0530

    itr

---
+++ src/com/java/optionalClass/Optional_Ex.java
@@ -0,0 +1,23 @@
+package com.java.optionalClass;
+
+import java.util.Optional;
+
+public class Optional_Ex 
+{
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) 
+	{
+		// Optional class is used to solve the NullPointerException
+		
+		int[] roll_No = {1,2,3,4,5,6,7,8,9};
+		System.out.println(roll_No.getClass());
+			for(Object a: roll_No)
+			{
+				System.out.println(a);
+			}
+		
+
+	}
+
+}

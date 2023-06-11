1af1e35308e7c024e7ad32eb9c2c4bf42e542b06
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-11 16:23:38 +0530

    ge

--- jdbcone/src/main/java/jdbcone/InsertData_Ex.java
+++ jdbcone/src/main/java/jdbcone/InsertData_Ex.java
@@ -15,13 +15,13 @@ public class InsertData_Ex {
 		//Create statement
 		Statement st = c.createStatement();
 			st.executeUpdate("insert into employe values (1,'Vasu',95123,'v@gmail.com')");
-//			st.executeUpdate("insert into employee values (2,'Balu',75123)");
+			st.executeUpdate("insert into employee values (2,'Balu',75123)");
 			st.executeUpdate("insert into employe values (3,'Ramu',65123,'r@gmail.com')");
-//			st.executeUpdate("insert into employee values (4,'Fanny',34123)");
+			st.executeUpdate("insert into employee values (4,'Fanny',34123)");
 			st.executeUpdate("insert into employe values (5,'Emma',94123,'e@gmail.com')");
-//			st.executeUpdate("insert into employee values (6,'Florence',84123)");
+			st.executeUpdate("insert into employee values (6,'Florence',84123)");
 			st.executeUpdate("insert into employe values (7,'Gene',74123,'g@gmail.com')");
-//			st.executeUpdate("insert into employee values (8,'Helen',34123)");
+			st.executeUpdate("insert into employee values (8,'Helen',34123)");
 			st.executeUpdate("insert into employe values (9,'Ian',97123,'i@gmail.com')");
 		
 		System.out.println("Data inserted successfully");

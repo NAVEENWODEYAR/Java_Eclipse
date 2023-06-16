201692d2b3af43418fb217adc1d45872087dd5a7
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-16 17:00:47 +0530

    ge

---
+++ jdbcone/src/main/java/exams/Inrement_Ex.java
@@ -0,0 +1,16 @@
+package exams;
+
+public class Inrement_Ex 
+{
+
+	public static void main(String[] args) 
+	{
+		// Write the output of the program..,
+		int i = 0;
+		int j = i;
+		j = i++ +j;
+		System.out.println(i+"  "+j);
+
+	}
+
+}

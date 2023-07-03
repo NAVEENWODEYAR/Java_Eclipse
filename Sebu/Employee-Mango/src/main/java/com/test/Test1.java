c62630b2c711587ed1ef87c01ac52c5c50084dfa
Naveen <naveenkrishnawodeyar@gmail.com>
2023-07-03 17:49:40 +0530

    ge

---
+++ Sebu/Employee-Mango/src/main/java/com/test/Test1.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Test1 
+{
+
+	public static void main(String[] args) 
+	{
+	// testing the toString()
+		System.out.println(new Test1());
+
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Boss";
+	}
+}

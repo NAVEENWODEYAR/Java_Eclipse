3aa857731b21550dc37075398699c565abe81804
Naveen <naveenkrishnawodeyar@gmail.com>
2023-07-05 17:14:45 +0530

    ge

---
+++ Sebu/Today/ui/bookstore/src/JS/emp.js
@@ -0,0 +1,43 @@
+
+let emp = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      }
+]
+
+let e =    {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  }
+console.log(emp);
+
+Object.entries(e).map(e=>console.log(e));

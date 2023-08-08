972e99eaa2472867c14c485de9e37305c7c6484c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-08 19:35:54 +0530

    bean

--- Sebu/Swagger_Demo/src/main/java/com/swager/config/SwagerConfig.java
+++ Sebu/Swagger_Demo/src/main/java/com/swager/config/SwagerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwagerConfig
 {
-
+	// Documentation object creation,
 	@Bean
 	public Docket api()
 	{

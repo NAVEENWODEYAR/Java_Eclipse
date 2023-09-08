67c62050cb40ea777c6a300daca975a554f3709d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-09 02:02:41 +0530

    profiles

--- DataJPA/src/main/java/com/jpa/DataJpaApplication.java
+++ DataJPA/src/main/java/com/jpa/DataJpaApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataJpaApplication {
+public class DataJpaApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DataJpaApplication.class, args);
+		System.out.println("Application started,");
 	}
 
 }

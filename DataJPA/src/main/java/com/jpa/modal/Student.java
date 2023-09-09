b2501b559ac46bdbd565e2c51249cca520f553c2
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-10 01:00:42 +0530

    @Column

--- DataJPA/src/main/java/com/jpa/modal/Student.java
+++ DataJPA/src/main/java/com/jpa/modal/Student.java
@@ -1,7 +1,11 @@
 package com.jpa.modal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +14,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "map_Student")
 public class Student 
 {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Student Id")
 	private int stId;
+	
+	@Column(name = "Student Name")
 	private String stName;
+	
+	@Column(name = "Student Address")
 	private String stAddress;
+	
+	@Column(name = "Student Mail Id")
+	private String stMail;
+	
+	
 }

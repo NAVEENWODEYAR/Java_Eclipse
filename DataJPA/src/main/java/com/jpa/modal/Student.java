b5dc0880e7de61e58bf838a07137e64677f1f28b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-09 09:39:44 +0530

    UUID

--- DataJPA/src/main/java/com/jpa/modal/Student.java
+++ DataJPA/src/main/java/com/jpa/modal/Student.java
@@ -2,8 +2,22 @@ package com.jpa.modal;
 
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student 
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int stId;
 	private UUID stSSN = UUID.randomUUID();
 	private String stName;

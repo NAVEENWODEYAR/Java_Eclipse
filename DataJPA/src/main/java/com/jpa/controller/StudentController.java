5dd9dd4af960053a609048d4f8efe0a8e8b025b4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-09 09:43:01 +0530

    @GetMapping

--- DataJPA/src/main/java/com/jpa/controller/StudentController.java
+++ DataJPA/src/main/java/com/jpa/controller/StudentController.java
@@ -1,9 +1,24 @@
 package com.jpa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jpa.service.StudentService;
+
 @RestController
+@RequestMapping("/student")
 public class StudentController
 {
-
+	// Dependency Injection(constructor)
+	@Autowired
+	private StudentService studentService;
+	
+	// API,
+	@GetMapping("/testMsg")
+	public String testMsg()
+	{
+		return "Mapping in JPA,";
+	}
 }

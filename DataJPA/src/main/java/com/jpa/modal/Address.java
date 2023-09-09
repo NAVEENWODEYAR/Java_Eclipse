f0da4464b92e6ae3bae1044b1eee739750184ea2
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-10 01:40:04 +0530

    error

--- DataJPA/src/main/java/com/jpa/modal/Address.java
+++ DataJPA/src/main/java/com/jpa/modal/Address.java
@@ -1,5 +1,6 @@
 package com.jpa.modal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Address
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "addressId")
 	private int addressId;
 	
 	private String address;

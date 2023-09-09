de3bbaecbff8b9fef97a80a200f2628b9a4a0a90
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-09 11:17:40 +0530

    modal class removed

--- DataJPA/src/main/java/com/jpa/modal/Student.java
+++ DataJPA/src/main/java/com/jpa/modal/Student.java
@@ -1,24 +1,6 @@
 package com.jpa.modal;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Student 
 {
-	@Id
-	private int stId;
-	private int stSSN;
-	private String stName;
-	private String stClass;
-	private String stAddress;
+
 }

88ad8ba974cbaefd4a62afc811437042c446ffef
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-09 09:31:17 +0530

    gm

--- DataJPA/src/main/java/com/jpa/modal/Student.java
+++ DataJPA/src/main/java/com/jpa/modal/Student.java
@@ -1,9 +1,12 @@
-/**
- * 
- */
 package com.jpa.modal;
+
+import java.util.UUID;
 
 public class Student 
 {
-
+	private int stId;
+	private UUID stSSN = UUID.randomUUID();
+	private String stName;
+	private String stClass;
+	private String stAddress;
 }

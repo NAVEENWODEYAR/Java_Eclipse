1cfacdd2ca2923f3fcc119c7f257d36a5b0898d5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-11 00:54:08 +0530

    java.sql.SQLSyntaxErrorException:

--- DataJPA/src/main/java/com/jpa/repo/StudentRepo.java
+++ DataJPA/src/main/java/com/jpa/repo/StudentRepo.java
@@ -15,7 +15,7 @@ public interface StudentRepo extends JpaRepository<Student, Integer>
 	Student findByFaNameAndLaName(String fName, String lName);
 
 	// JPQL queries
-	@Query(value = "SELECT * FROM Student s ORDER BY s.First Name",nativeQuery = true)
+	@Query(value = "SELECT * FROM Student s ORDER BY s.faName",nativeQuery = true)
 	List<Student> getStudentOrderByfirstName();
 	
 //	@Query(value = "SELECT s FROM Student s WHERE s.Student MailId=?1")

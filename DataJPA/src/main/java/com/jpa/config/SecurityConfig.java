ccdcdc5d8af5db85adddf1d829010957fb351137
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-10 14:19:08 +0530

    403

--- DataJPA/src/main/java/com/jpa/config/SecurityConfig.java
+++ DataJPA/src/main/java/com/jpa/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
-@Configuration
 @Component
 public class SecurityConfig 
 {

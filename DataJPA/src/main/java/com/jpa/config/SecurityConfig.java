58fd87b3e4afb351ba411e73fd4fbc690315335f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-10 14:09:47 +0530

    ga

--- DataJPA/src/main/java/com/jpa/config/SecurityConfig.java
+++ DataJPA/src/main/java/com/jpa/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.jpa.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
+@Configuration
 @Component
 public class SecurityConfig 
 {

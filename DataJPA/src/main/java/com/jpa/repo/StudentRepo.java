418ba64f9d68720e4ac664f53953a50dba9fa601
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-10 16:34:25 +0530

    Student findByFaName(String faName);

--- DataJPA/src/main/java/com/jpa/repo/StudentRepo.java
+++ DataJPA/src/main/java/com/jpa/repo/StudentRepo.java
@@ -8,9 +8,9 @@ import com.jpa.modal.Student;
 
 public interface StudentRepo extends JpaRepository<Student, Integer> 
 {
-	Student findByfName(String fName);
-	List<Student> findByfNameContaining(String name);
-	List<Student> findBylNameNotNull();
+	Student findByFaName(String faName);
+	List<Student> findByFaNameContaining(String name);
+	List<Student> findByLaNameNotNull();
 //	Student findByfNameAndlName(String fName, String lName);
 	Student findByFName(String fName);
 }

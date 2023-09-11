0951170a4b32e10defdba69880dfd91a01a346d9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-11 12:30:02 +0530

    map_Student

--- DataJPA/src/main/java/com/jpa/repo/StudentRepo.java
+++ DataJPA/src/main/java/com/jpa/repo/StudentRepo.java
@@ -15,7 +15,7 @@ public interface StudentRepo extends JpaRepository<Student, Integer>
 	Student findByFaNameAndLaName(String fName, String lName);
 
 	// JPQL queries
-	@Query(value = "SELECT * FROM Student  ORDER BY Student.faName",nativeQuery = true)
+	@Query(value = "SELECT * FROM map_Student  ORDER BY map_Student.faName",nativeQuery = true)
 	List<Student> getStudentOrderByfirstName();
 	
 //	@Query(value = "SELECT s FROM Student s WHERE s.Student MailId=?1")
